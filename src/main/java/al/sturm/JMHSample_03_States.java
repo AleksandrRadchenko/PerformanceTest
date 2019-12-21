9e862ed256eb7c1ab541d9c1bccf8e7ad1554bef
Alexander Radchenko <partner.ar@gmail.com>
2019-12-22 02:22:45 +0300

    Add state example

---
+++ src/main/java/al/sturm/JMHSample_03_States.java
@@ -0,0 +1,72 @@
+package al.sturm;
+
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.State;
+import org.openjdk.jmh.runner.Runner;
+import org.openjdk.jmh.runner.RunnerException;
+import org.openjdk.jmh.runner.options.Options;
+import org.openjdk.jmh.runner.options.OptionsBuilder;
+import org.openjdk.jmh.runner.options.TimeValue;
+
+/**
+ * https://hg.openjdk.java.net/code-tools/jmh/file/c8f9f5b85cd9/jmh-samples/src/main/java/org/openjdk/jmh/samples/JMHSample_03_States.java
+ * You are expected to see the drastic difference in shared and unshared cases,
+ * because you either contend for single memory location, or not. This effect
+ * is more articulated on large machines.
+ */
+@SuppressWarnings("squid:S00101")
+public class JMHSample_03_States {
+    public static void main(String[] args) throws RunnerException {
+        Options opt = new OptionsBuilder()
+                .include(JMHSample_03_States.class.getSimpleName())
+                .threads(4)
+                .forks(1)
+                .warmupIterations(2)
+                .warmupTime(TimeValue.seconds(1))
+                .measurementIterations(3)
+                .measurementTime(TimeValue.seconds(3))
+                .build();
+
+        new Runner(opt).run();
+    }
+
+    @State(Scope.Benchmark)
+    public static class BenchmarkState {
+        volatile double x = Math.PI;
+    }
+
+    @State(Scope.Thread)
+    public static class ThreadState {
+        volatile double x = Math.PI;
+    }
+
+    /*
+     * Benchmark methods can reference the states, and JMH will inject the
+     * appropriate states while calling these methods. You can have no states at
+     * all, or have only one state, or have multiple states referenced. This
+     * makes building multi-threaded benchmark a breeze.
+     *
+     * For this exercise, we have two methods.
+     */
+
+    @Benchmark
+    public void measureUnshared(ThreadState state) {
+        // All benchmark threads will call in this method.
+        //
+        // However, since ThreadState is the Scope.Thread, each thread
+        // will have it's own copy of the state, and this benchmark
+        // will measure unshared case.
+        state.x++;
+    }
+
+    @Benchmark
+    public void measureShared(BenchmarkState state) {
+        // All benchmark threads will call in this method.
+        //
+        // Since BenchmarkState is the Scope.Benchmark, all threads
+        // will share the state instance, and we will end up measuring
+        // shared case.
+        state.x++;
+    }
+}
